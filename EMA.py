29493ce9e23ba8033297e180f56dc7decc313d4a
Nolan Thompson <nolanthompson@Nolans-MacBook-Pro.local>
2020-12-22 16:59:06 -0500

    Added Test Dates

--- EMA.py
+++ EMA.py
@@ -1,2 +1,10 @@
 #Method to calculate EMA
+import pandas.io.data as web
+import datetime
 
+start = datetime.datetime(2018, 1, 1)
+end = datetime.datetime(2020, 1, 1)
+
+test = web.DataReader("AAPL", "yahoo", start, end)
+
+print(test)
