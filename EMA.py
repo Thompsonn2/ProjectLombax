70a8c4d9bb305c91d1f291fca48de787058f5d2e
Nolan Thompson <nolanthompson@Nolans-MacBook-Pro.local>
2020-12-28 16:44:33 -0500

    Alpha Vantage as new Database

--- EMA.py
+++ EMA.py
@@ -5,12 +5,16 @@ import datetime
 start = datetime.datetime(2018, 1, 1)
 end = datetime.datetime(2020, 1, 1)
 
-ticker = "AAPL"
+API_KEY = '8YUCTMX2NCJUI70T'
 
-test = web.DataReader(ticker, "yahoo", start, end)
+ticker = 'AAPL'
+
+test = web.DataReader(ticker, 'av-daily-adjusted', start, end, api_key = API_KEY)
 
 #print(test)
 
 csv_name = ticker + '.csv'
+
+print(test)
 
 test.to_csv(csv_name)
