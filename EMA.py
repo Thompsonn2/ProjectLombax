d51a74acb763459a897f1d3d81e4e4bc3180409f
Nolan <thompsonn2@allegheny.edu>
2020-12-28 17:51:20 -0500

    Calculated EMA

--- EMA.py
+++ EMA.py
@@ -1,5 +1,6 @@
 #Method to calculate EMA
 import pandas_datareader as web
+import pandas as pd
 import datetime
 
 start = datetime.datetime(2018, 1, 1)
@@ -10,11 +11,44 @@ API_KEY = '8YUCTMX2NCJUI70T'
 ticker = 'AAPL'
 
 test = web.DataReader(ticker, 'av-daily-adjusted', start, end, api_key = API_KEY)
-
 #print(test)
 
 csv_name = ticker + '.csv'
 
-print(test)
-
 test.to_csv(csv_name)
+
+pricedata = pd.read_csv(csv_name)
+
+acprices = pricedata.loc[:,'adjusted close']
+#print(acprices)
+
+#12 Day EMA
+
+emalng = 12
+acptotal = 0
+
+#12 Day SMA (intial EMA)
+for i in range(emalng):
+    acptotal = acprices[i] + acptotal
+
+sma = acptotal / emalng
+print(sma)
+
+#Multiplier
+multi = 2 / (acptotal + 1)
+print(multi)
+
+#Closing Price
+acp = acprices[emalng - 1]
+print(acp)
+
+#EMA
+ema = ((acp - sma) * multi) + sma
+print(ema)
+
+
+
+
+
+
+
